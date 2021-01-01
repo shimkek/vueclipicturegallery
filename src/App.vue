876d08bb97b79fda7800a382bdfcfa75f7c9ffff
Shimkek <maksim.dunaitsev@gmail.com>
2021-01-02 03:40:06 +0600

    Working vue cli setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="header">
+        <div class="header__contents">
+            <a class="header__logo" href="./index.html">
+                <img class="header__pic" src="./icons/android-chrome-192x192.png" alt="logo">
+            </a>
+            <p class="header__name">Gallery</p>
+        </div>
+    </header>
+
+    <div class="gallery" :class="{gallery_minimized : isMaximized}">
+        <gallery-poster v-for="(poster, index) in gallery" :poster="poster" :key="index" @maximize-poster="showMaximized"></gallery-poster>
+    </div>
+
+    <div class="loadmore" :class="{loadmore_minimized : isMaximized}">
+        <button class="loadmore__button" @click="fetchImages">
+            ↻ Load more
+        </button>
+    </div>
+
+    <maximized v-if="isMaximized" :poster="maximizedPoster" @hide-maximized-event="hideMaximized"></maximized>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GalleryPoster from './components/GalleryPoster.vue'
+import Maximized from './components/Maximized.vue'
 
 export default {
-  name: 'App',
+  data () {
+    return {
+      pictureQuntity: 0,
+      page: 0,
+      gallery: [],
+      isMaximized: false,
+      maximizedPoster: {
+        name: null,
+        district: null,
+        coordinates: null,
+        src: null
+      }
+    }
+  },
+  methods: {
+    async fetchImages () {
+      this.page++
+      this.pictureQuntity = this.pictureQuntity + 6
+      const response = await fetch(this.fetchUrl)
+      const posters = await response.json()
+      console.log(posters)
+      for (let i = 0; i < posters.data.length; i++) {
+        this.gallery.push(posters.data[i])
+      }
+    },
+    showMaximized (poster) {
+      this.maximizedPoster.name = poster.name
+      this.maximizedPoster.coordinates = poster.coordinates
+      this.maximizedPoster.district = poster.district
+      this.maximizedPoster.src = poster.src
+      this.isMaximized = true
+    },
+    hideMaximized () {
+      this.isMaximized = false
+    }
+  },
+  computed: {
+    fetchUrl () {
+      return 'https://okolo.city/api/v1/subjects?only_paid=0&page=' + this.page + '&per_page=6' + '&pagination_hash_random=1'
+    }
+  },
+  async mounted () {
+    this.fetchImages()
+  },
   components: {
-    HelloWorld
+    GalleryPoster,
+    Maximized
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './styles/index.scss';
 </style>
---
+++ src/components/GalleryPoster.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="gallery__poster" @click="maximize">
+    <img class="gallery__img" :src="this.poster.preview">
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    poster: {}
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    maximize () {
+      const poster = {
+        name: this.poster.name,
+        coordinates: this.poster.coordinates_geo_object,
+        district: this.poster.district,
+        src: this.poster.preview
+      }
+      console.log(poster)
+      this.$emit('maximize-poster', poster)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.gallery__poster {
+  cursor: pointer;
+  width: 480px;
+  height: 270px;
+  margin: 20px;
+  box-shadow: 4px 4px 20px;
+  box-sizing: border-box;
+}
+.gallery__img {
+  width: inherit;
+  object-fit: contain;
+}
+</style>
